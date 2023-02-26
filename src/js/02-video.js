1925db6949c26d8cc5b6ac447460b965fc0895d9
Bestis21 <zaichkodmytro2004@gmail.com>
2023-02-26 15:08:25 +0100

    finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = "videoplayer-current-time"
+
+player.on('timeupdate', throttle(OnPlayerData, 1000));
+function OnPlayerData(data) {
+    localStorage.setItem(LOCAL_KEY, data.seconds)
+} 
+const PlayerCurrentValue = localStorage.getItem(LOCAL_KEY)
+if (PlayerCurrentValue) {
+player.setCurrentTime(PlayerCurrentValue)
+}
